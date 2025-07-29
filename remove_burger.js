fafed49f04ea66151ea1483d092652476b7677c7
Vivek <vgamit637@gmail.com>
2025-07-29 07:15:52 +0000

    remove element usining splice

---
+++ remove_burger.js
@@ -0,0 +1,33 @@
+// You just need to implement the filterHealthy function
+function filterHealthy(foodList) {
+    // Remove unhealthy food and return updated list
+   
+    const index = foodList.indexOf("burger");
+
+    if(index !== -1){
+        foodList.splice(index,1);
+    }
+    return foodList;
+}
+
+
+// Please don't remove the code below
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (input) => {
+  // Parse input (expected to be JSON string format),
+  // which should contain foodList
+  const { foodList } = JSON.parse(input);
+
+  // Call our function
+  const result = filterHealthy(foodList);
+
+  // Output the result as a JSON string
+  process.stdout.write(JSON.stringify(result));
+});
+// Please don't remove the code above
