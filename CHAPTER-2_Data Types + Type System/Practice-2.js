a69082ed8cfd180c435372465248f3d30ea0d5dd
Vivek <vgamit637@gmail.com>
2025-08-19 21:25:39 +0530

    Create Practice-2.js

---
+++ CHAPTER-2_Data Types + Type System/Practice-2.js
@@ -0,0 +1,8 @@
+console.log(typeof []);     //object
+console.log(typeof null);   //object
+console.log(typeof {});     //object
+console.log(typeof 1);      //number
+console.log(typeof undefined);  //undefined
+console.log(typeof "herry");    //string    
+console.log(typeof true);       //boolean
+console.log(typeof NaN);        //number
