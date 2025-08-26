eadf4c5607f60b4b1537015cb9d2bf37a7dcf4ff
Vivek <vgamit637@gmail.com>
2025-08-26 09:09:21 +0530

    Create Practice-6.js

---
+++ CHAPTER-7_Arrays/Practice-6.js
@@ -0,0 +1,9 @@
+//Sort array of ages in ascending order
+let age = [23,54,78,13,55];
+let sortasc = age.sort((a,b)=>a-b);
+console.log(sortasc);
+
+console.log("")
+console.log("decending")
+let sortdec = age.sort((a,b)=>b-a);
+console.log(sortdec);
