5f3b60d91cbe92a15b682abc4cba8b070c3c3815
Vivek <vgamit637@gmail.com>
2025-08-26 09:07:25 +0530

    Create Practice-1.js

---
+++ CHAPTER-7_Arrays/Practice-1.js
@@ -0,0 +1,16 @@
+//Create an array of student names and print each
+
+let arr = ["student-1","student-2","student-3","student-4","student-5"];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+
+console.log("");
+//Filter even numbers from an array
+
+let numarr = [1,32,54,232,66,34,78,86];
+
+let even = numarr.filter(n => n%2===0);
+console.log(even);
